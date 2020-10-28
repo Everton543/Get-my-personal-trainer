85e410528302373531c4b6f718c0f5c7ceef5fc2
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2020-10-28 16:39:00 -0700

    Add additional screens

---
+++ app/src/main/java/com/example/getmypersonaltrainer/RemoveClientInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.getmypersonaltrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemoveClientInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_remove_client_info);
+    }
+}
