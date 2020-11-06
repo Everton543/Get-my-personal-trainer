9ecf2f7fce4fa219129afdfb5a61cd0b15e55e36
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2020-11-05 20:26:12 -0800

    Adding screens and text fields.

---
+++ app/src/main/java/com/example/getmypersonaltrainer/PersonalTrainerInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.getmypersonaltrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalTrainerInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_trainer_info);
+    }
+}
