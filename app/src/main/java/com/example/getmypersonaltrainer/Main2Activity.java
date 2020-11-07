a0192d0f3a61568aa33eaac714a8142988cc0627
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2020-11-06 20:27:28 -0800

    Adding screens and text fields. Work on personal trainer Add more screens and text fields..

--- app/src/main/java/com/example/getmypersonaltrainer/Main2Activity.java
+++ app/src/main/java/com/example/getmypersonaltrainer/Main2Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PersonalTrainingActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_personal_training);
+        setContentView(R.layout.activity_main2);
     }
 }
