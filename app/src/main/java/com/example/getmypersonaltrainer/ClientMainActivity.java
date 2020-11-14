da1e9bafa61021d31ea425b6e8be3cf4e6712efc
everton543 <everton.felipe.alves@gmail.com>
2020-11-13 23:47:01 -0300

    Correct design client main activity, personal trainer info activity. Create Client Menu

--- app/src/main/java/com/example/getmypersonaltrainer/ClientMainActivity.java
+++ app/src/main/java/com/example/getmypersonaltrainer/ClientMainActivity.java
@@ -3,6 +3,8 @@ package com.example.getmypersonaltrainer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class ClientMainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class ClientMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.client_menu, menu);
+        return true;
     }
 }
