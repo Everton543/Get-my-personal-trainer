9a773fd5c304660f53eb15f9120eed7e5586de85
Jade <jadencamargo@gmail.com>
2020-11-28 13:45:49 -0700

    Added unsubscribeFromPersonalTrainer to client, then added the ability for the client to see his list of activities, and exercise info for all his/her exercises including the days of the week they should practice them. Jaden

--- app/src/main/java/com/example/getmypersonaltrainer/Model.java
+++ app/src/main/java/com/example/getmypersonaltrainer/Model.java
@@ -22,6 +22,7 @@ import java.util.Objects;
 
 import static com.example.getmypersonaltrainer.MainActivity.presenter;
 
+//
 
 public class Model {
    private FirebaseDatabase database = null;
@@ -38,6 +39,22 @@ public class Model {
    public Model(){
       database = FirebaseDatabase.getInstance();
       databaseReference = database.getReference();
+      // Read from the database
+      databaserf.addValueEventListener(new ValueEventListener() {
+         @Override
+         public void onDataChange(DataSnapshot dataSnapshot) {
+            // This method is called once with the initial value and again
+            // whenever data at this location is updated.
+            String value = dataSnapshot.getValue(String.class);
+            Log.d(TAG, "Value is: " + value);
+         }
+
+         @Override
+         public void onCancelled(DatabaseError error) {
+            // Failed to read value
+            Log.w(TAG, "Failed to read value.", error.toException());
+         }
+      });
    }
 
    ValidateInfo getValidateInfo(){
