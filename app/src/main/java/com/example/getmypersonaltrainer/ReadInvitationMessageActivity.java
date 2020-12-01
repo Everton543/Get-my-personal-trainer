6d496ebbea75ca80688a3679dfd0ea569a27d616
everton543 <everton.felipe.alves@gmail.com>
2020-11-30 23:09:56 -0300

    Created ReadInvitationMessageActivity

---
+++ app/src/main/java/com/example/getmypersonaltrainer/ReadInvitationMessageActivity.java
@@ -0,0 +1,14 @@
+package com.example.getmypersonaltrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadInvitationMessageActivity extends AppCompatActivity {
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_read_invitation_message);
+   }
+}
