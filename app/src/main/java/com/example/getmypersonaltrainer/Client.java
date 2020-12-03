d5ce10fa4ceaeac2056ca757b0f8c31967748471
Cheri Hansen <72418856+coolcheri87@users.noreply.github.com>
2020-12-02 20:22:03 -0800

    Switch CalendarView to Spinners

--- app/src/main/java/com/example/getmypersonaltrainer/Client.java
+++ app/src/main/java/com/example/getmypersonaltrainer/Client.java
@@ -1,7 +1,10 @@
 package com.example.getmypersonaltrainer;
 
+import android.util.Log;
+
 import java.time.DayOfWeek;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,6 +25,7 @@ public class Client implements UserInterface, ClientInterface{
    private boolean receivedInvitation = false;
    private String personalTrainerId;
    private boolean voted = false;
+   private static final String TAG = "Client";
 
    public Client(){}
 
@@ -233,11 +237,101 @@ public class Client implements UserInterface, ClientInterface{
       return true;
    }
 
-   public int getAge()
-   {
-      //TO-DO: FIX THIS
-      return 100;
+   public int getAge() {
+      int age = 969; // Default to the age of Methusaleh
 
+      // Validate birthDate...
+      boolean error = false;
+      String birthDate = getBirthDate();
+      if ((birthDate==null)||(birthDate.length()<10)||
+              (birthDate.charAt(4)!='/')||(birthDate.charAt(7)!='/')) {
+         error = true;
+         Log.i(TAG, "getAge() - birthDate null, insufficient length, or no slashes.");
+      }
+
+      // Validate that there are only numbers in the right places...
+      String[] ymd = { null, null, null };
+      if (!error) {
+         // Need to validate that we only have numbers
+         ymd[0] = birthDate.substring(0, 4);
+         ymd[1] = birthDate.substring(5, 7);
+         ymd[2] = birthDate.substring(8,10);
+         String test = ymd[0] + ymd[1] + ymd[2];
+         String legal = "0123456789";
+         for (int i=0;i<test.length();++i) {
+            boolean found = false;
+            for (int j=0;j<legal.length();++j)
+               if (test.charAt(i)==legal.charAt(j))
+                  found = true;
+            if (!found)
+               error = true;
+         }
+         if (error)
+            Log.i(TAG, "getAge() - birthDate doesn't have all numbers formatted correctly.");
+      }
+
+      // Parse the Strings into integers...
+      int[] birthday = { 0, 0, 0 };
+      if (!error) {
+         try {
+            birthday[0] = Integer.parseInt(ymd[0]);
+            birthday[1] = Integer.parseInt(ymd[1]);
+            birthday[2] = Integer.parseInt(ymd[2]);
+         }
+         catch(NumberFormatException e) {
+            error = true;
+            Log.i(TAG, "getAge() - parseInt error: " + e.getMessage());
+         }
+      }
+
+      // Get today's date and test validity of parsed date.
+      GregorianCalendar gc = new GregorianCalendar();
+      int year = gc.get(GregorianCalendar.YEAR);
+      int month = gc.get(GregorianCalendar.MONTH) + 1;
+      int day = gc.get(GregorianCalendar.DAY_OF_MONTH);
+      if (!error) {
+         // Test valid dates...
+         if (birthday[0] < 1900) error = true;
+         if ((birthday[1] < 1) || (birthday[2] < 1)) error = true;
+         if ((birthday[1] > 12) || (birthday[2] > 31)) error = true;
+         if (birthday[0] > year) error = true;
+         if ((birthday[0] == year) && (birthday[1] > month)) error = true;
+         if ((birthday[0] == year) && (birthday[1] == month) && (birthday[2] >= day)) error = true;
+
+         // Check specific month lengths...
+         switch (birthday[1]) {
+            case 2: // Feb
+               if (((birthday[0] % 4) == 0) && (birthday[2] > 29)) error = true;
+               if (((birthday[0] % 4) > 0) && (birthday[2] > 28)) error =true;
+               break;
+            case 4: // Mar
+            case 6: // Jun
+            case 9: // Sep
+            case 11: // Nov
+               if (birthday[2] > 30) error = true;
+            default:
+               break;
+         }
+         if (error)
+            Log.i(TAG, "getAge() - birthDate is an illegal date!");
+      }
+
+      // If there are no errors, we can now calculate the age.
+      if (!error) {
+         age = year - birthday[0];
+         if (month<birthday[1]) {
+            --age;
+         }
+         if ((month==birthday[1])&&(day<birthday[2])) {
+            --age;
+         }
+      }
+
+      // Logcat error that the age will be 969!
+      if (error)
+         Log.i(TAG, "getAge() - He must be Methusaleh as his age is 969!");
+
+      return age;
    }
 
    public List<Exercise> getExercisesOfWeekDay(DayOfWeek dayOfWeek){
--- app/src/main/java/com/example/getmypersonaltrainer/ClientSignUpActivity.java
+++ app/src/main/java/com/example/getmypersonaltrainer/ClientSignUpActivity.java
@@ -1,24 +1,64 @@
 package com.example.getmypersonaltrainer;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.CalendarView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import android.widget.Spinner;
+import java.text.NumberFormat;
 import java.util.GregorianCalendar;
 
-public class ClientSignUpActivity extends AppCompatActivity{
+
+public class ClientSignUpActivity extends AppCompatActivity {
+    public static String TAG = "ClientSignUpActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_sign_up);
         MainActivity.presenter.setActualActivity(this);
+
+        // Setup spinners for the birthdate picker...
+        // Setup arrays to pick from...
+        String[] days = {
+                "01", "02", "03", "04", "05", "06", "07", "08", "09", "10",
+                "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                "31"
+        };
+        String[] months = {
+                "01", "02", "03", "04", "05", "06",
+                "07", "08", "09", "10", "11", "12"
+        };
+        GregorianCalendar gc = new GregorianCalendar();
+        String[] years = new String[gc.get(GregorianCalendar.YEAR)-1900+1];
+        for (int i=0;i<years.length;++i)
+            years[i] = Integer.toString(1900 + i);
+
+        // Get our spinners and setup with arrays
+        Spinner spinnerYear = (Spinner) findViewById(R.id.spinnerYear);
+        Spinner spinnerMonth = (Spinner) findViewById(R.id.spinnerMonth);
+        Spinner spinnerDay = (Spinner) findViewById(R.id.spinnerDay);
+        // Put the arrays into the spinners...
+        ArrayAdapter<String> adapterYear = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, years);
+        ArrayAdapter<String> adapterMonth = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, months);
+        ArrayAdapter<String> adapterDay = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, days);
+        // Specify the layout to use when the list of choices appears
+        adapterYear.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterMonth.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterDay.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerYear.setAdapter(adapterYear);
+        spinnerMonth.setAdapter(adapterMonth);
+        spinnerDay.setAdapter(adapterDay);
+
+        // Set the selection to 20 years ago...
+        spinnerYear.setSelection(gc.get(GregorianCalendar.YEAR)-1920);
+        spinnerMonth.setSelection(gc.get(GregorianCalendar.MONTH));
+        spinnerDay.setSelection(gc.get(GregorianCalendar.DAY_OF_MONTH)-1);
     }
 
     public void ClientSignUp(View view){
@@ -38,12 +78,50 @@ public class ClientSignUpActivity extends AppCompatActivity{
         editText = (EditText) findViewById(R.id.edit_text_phone_client_sign_up_activity);
         String phone = editText.getText().toString();
 
-        CalendarView CV = (CalendarView) findViewById(R.id.select_cv_birth_date_client_sign_up_activity);
-        long gcLong = CV.getDate();
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.setTime(new Date(gcLong));
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        String birthDate = sdf.format(gc);
+        // Setup CalendarView object...
+        Spinner spinnerYear = (Spinner) findViewById(R.id.spinnerYear);
+        Spinner spinnerMonth = (Spinner) findViewById(R.id.spinnerMonth);
+        Spinner spinnerDay = (Spinner) findViewById(R.id.spinnerDay);
+        String YYYY = (String)spinnerYear.getSelectedItem();
+        String MM = (String)spinnerMonth.getSelectedItem();
+        String DD = (String)spinnerDay.getSelectedItem();
+        int yyyy = Integer.parseInt(YYYY);
+        int mm = Integer.parseInt(MM);
+        int dd = Integer.parseInt(DD);
+        // Verify day of the month...
+        // Check specific month lengths...
+        boolean changed = false;
+        switch (mm) {
+            case 2: // Feb
+                if (((yyyy % 4) == 0) && (dd > 29)) {
+                    dd = 29;
+                    changed = true;
+                }
+                if (((yyyy % 4) > 0) && (dd > 28)) {
+                    dd = 28;
+                    changed = true;
+                }
+                break;
+            case 4: // Mar
+            case 6: // Jun
+            case 9: // Sep
+            case 11: // Nov
+                if (dd > 30) {
+                    dd = 30;
+                    changed = true;
+                }
+            default:
+                break;
+        }
+        if (changed) {
+            NumberFormat nf2 = NumberFormat.getInstance();
+            nf2.setMinimumIntegerDigits(2);
+            spinnerDay.setSelection(dd-1);
+            DD = nf2.format(dd);
+        }
+        // Set our global birthDate field...
+        String birthDate = YYYY + "/" + MM + "/" + DD;
+        Log.i(TAG,"Setting birthDate to: " + birthDate);
 
         editText = (EditText) findViewById(R.id.edit_text_weight_client_sign_up_activity);
         String textBodyMass = editText.getText().toString();
